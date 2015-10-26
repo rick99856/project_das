a75065f7977104aa2daeca8ae2555d69b1935b75
rick99856 <zx0956950105>
2015-10-26 20:17:53 +0800

    disease_new

--- disease_new.php
+++ disease_new.php
@@ -1,36 +1,46 @@
 <?php
-$mysql_db_hostname = "localhost";
+$mysql_db_hostname = "192.168.137.178";
 $mysql_db_user = "s13113241";
 $mysql_db_password = "hs9m322x";
 $mysql_db_database = "project_das";
 
 
 $con = mysql_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password);
-// !empty()
-if (!$con) {
- die('Could not connect: ' . mysql0erropr());
-}
+// if (!empty($_POST['name']) && !empty($_POST['danger'])&& !empty($_POST['infection'])
+// 	&& !empty($_POST['info'])&& !empty($_POST['population'])&& !empty($_POST['symptom'])
+// 	&& !empty($_POST['prevention'])&& !empty($_POST['treatment'])) {
+	
+// }
+// else{
+// 	echo "yyy";
+// }
+// if (!$con) {
+//  die('Could not connect: ' . mysql_error());
+// }
 mysql_query("set names 'utf8'");
 
 
 mysql_select_db($mysql_db_database);
-   	$sql = "SELECT * FROM food join cag on food.cagID = cag.cagID";
-    $result = mysql_query($sql) or die('MySQL query error');
+$sql = "INSERT INTO `project_das`.`dis` (`no`, `name`, `danger`, `infection`, `info`, `population`, `symptom`, `prevention`, `treatment`) 
+   		VALUES ( NULL,'','','','','','','','') ";
+
+    $result = mysql_query($sql) or die(mysql_error());
     $i = 0;
+    echo $result;
+ //    $json = array();
+ //    while($row = mysql_fetch_array($result)){
+ //        $json[$i]['name'] = $row['name'];
+	// 	$json[$i]['danger'] = $row['danger'];
+	// 	$json[$i]['infection'] = $row['infection'];
+	// 	$json[$i]['info'] = $row['info'];
+	// 	$json[$i]['population'] = $row['population'];
+	// 	$json[$i]['symptom'] = $row['symptom'];
+	// 	$json[$i]['prevention'] = $row['prevention'];
+	// 	$json[$i]['treatment'] = $row['treatment'];
 
-     $json = array();
-    while($row = mysql_fetch_array($result)){
-        $json[$i]['name'] = $row['name'];
-		$json[$i]['benefit'] = $row['benefit'];
-		$json[$i]['defect'] = $row['defect'];
-		$json[$i]['unfit'] = $row['unfit'];
-		$json[$i]['ps'] = $row['ps'];
-		$json[$i]['cagID'] = $row['cagID'];
-		$json[$i]['cagname'] = $row['cagname'];
-
-		$i++;
-    }
-	echo json_encode($json);
+	// 	$i++;
+ //    }
+	// echo json_encode($json);
 
 
 
